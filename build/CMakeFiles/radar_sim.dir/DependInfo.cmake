d86a28d8b56ae522f579e17e5d4b69dfd40bed9e
Mohit Pradhan <159414367+TsuKKi-is-dead@users.noreply.github.com>
2026-05-29 11:33:34 +0530

    final: complete Pulse-Doppler Radar Simulator project

---
+++ build/CMakeFiles/radar_sim.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/tsukki/PulseDopplerRadarSimulator/main.cpp" "CMakeFiles/radar_sim.dir/main.cpp.o" "gcc" "CMakeFiles/radar_sim.dir/main.cpp.o.d"
+  "/Users/tsukki/PulseDopplerRadarSimulator/src/Benchmark.cpp" "CMakeFiles/radar_sim.dir/src/Benchmark.cpp.o" "gcc" "CMakeFiles/radar_sim.dir/src/Benchmark.cpp.o.d"
+  "/Users/tsukki/PulseDopplerRadarSimulator/src/CFARDetector.cpp" "CMakeFiles/radar_sim.dir/src/CFARDetector.cpp.o" "gcc" "CMakeFiles/radar_sim.dir/src/CFARDetector.cpp.o.d"
+  "/Users/tsukki/PulseDopplerRadarSimulator/src/FFTProcessor.cpp" "CMakeFiles/radar_sim.dir/src/FFTProcessor.cpp.o" "gcc" "CMakeFiles/radar_sim.dir/src/FFTProcessor.cpp.o.d"
+  "/Users/tsukki/PulseDopplerRadarSimulator/src/KalmanTracker.cpp" "CMakeFiles/radar_sim.dir/src/KalmanTracker.cpp.o" "gcc" "CMakeFiles/radar_sim.dir/src/KalmanTracker.cpp.o.d"
+  "/Users/tsukki/PulseDopplerRadarSimulator/src/NoiseModel.cpp" "CMakeFiles/radar_sim.dir/src/NoiseModel.cpp.o" "gcc" "CMakeFiles/radar_sim.dir/src/NoiseModel.cpp.o.d"
+  "/Users/tsukki/PulseDopplerRadarSimulator/src/ScenarioGenerator.cpp" "CMakeFiles/radar_sim.dir/src/ScenarioGenerator.cpp.o" "gcc" "CMakeFiles/radar_sim.dir/src/ScenarioGenerator.cpp.o.d"
+  "/Users/tsukki/PulseDopplerRadarSimulator/src/SignalGenerator.cpp" "CMakeFiles/radar_sim.dir/src/SignalGenerator.cpp.o" "gcc" "CMakeFiles/radar_sim.dir/src/SignalGenerator.cpp.o.d"
+  "/Users/tsukki/PulseDopplerRadarSimulator/src/Visualizer.cpp" "CMakeFiles/radar_sim.dir/src/Visualizer.cpp.o" "gcc" "CMakeFiles/radar_sim.dir/src/Visualizer.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/radar_sim.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/radar_sim.dir/main.cpp.o"
+  "CMakeFiles/radar_sim.dir/main.cpp.o.d"
+  "CMakeFiles/radar_sim.dir/src/Benchmark.cpp.o"
+  "CMakeFiles/radar_sim.dir/src/Benchmark.cpp.o.d"
+  "CMakeFiles/radar_sim.dir/src/CFARDetector.cpp.o"
+  "CMakeFiles/radar_sim.dir/src/CFARDetector.cpp.o.d"
+  "CMakeFiles/radar_sim.dir/src/FFTProcessor.cpp.o"
+  "CMakeFiles/radar_sim.dir/src/FFTProcessor.cpp.o.d"
+  "CMakeFiles/radar_sim.dir/src/KalmanTracker.cpp.o"
+  "CMakeFiles/radar_sim.dir/src/KalmanTracker.cpp.o.d"
+  "CMakeFiles/radar_sim.dir/src/NoiseModel.cpp.o"
+  "CMakeFiles/radar_sim.dir/src/NoiseModel.cpp.o.d"
+  "CMakeFiles/radar_sim.dir/src/ScenarioGenerator.cpp.o"
+  "CMakeFiles/radar_sim.dir/src/ScenarioGenerator.cpp.o.d"
+  "CMakeFiles/radar_sim.dir/src/SignalGenerator.cpp.o"
+  "CMakeFiles/radar_sim.dir/src/SignalGenerator.cpp.o.d"
+  "CMakeFiles/radar_sim.dir/src/Visualizer.cpp.o"
+  "CMakeFiles/radar_sim.dir/src/Visualizer.cpp.o.d"
+  "radar_sim"
+  "radar_sim.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/radar_sim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
